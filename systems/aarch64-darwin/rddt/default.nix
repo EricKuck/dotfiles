3a817b25289232ed2485715899b0e6df6a9fcc56
EricKuck <eric@bluelinelabs.com>
2024-07-31 17:33:57 -0400

    Add rddt machine

---
+++ systems/aarch64-darwin/rddt/default.nix
@@ -0,0 +1,30 @@
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
+with lib.custom;
+{
+  custom = {
+    environments = {
+      common.enable = true;
+      android.enable = true;
+      ios.enable = true;
+      rust.enable = true;
+    };
+
+    programs = {
+      nh = {
+        enable = true;
+        flake = "path:/Users/erickuck/.config/nix";
+        clean = {
+          enable = true;
+          extraArgs = "--keep-since 4d --keep 3";
+        };
+      };
+    };
+  };
+
+  system.stateVersion = 4;
+}
