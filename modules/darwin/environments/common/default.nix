fb9827f1b9eef5bf908858e7c42284a8c13cc6f4
EricKuck <eric@bluelinelabs.com>
2024-09-20 09:55:15 -0400

    Add soduto

--- modules/darwin/environments/common/default.nix
+++ modules/darwin/environments/common/default.nix
@@ -161,6 +161,7 @@ in
         "docker"
         "mullvadvpn"
         "maccy"
+        "soduto"
         # Specific version of btt i have a license for. Continually zaps itself, so commented out until fixed.
         # "https://raw.githubusercontent.com/Homebrew/homebrew-cask/81a82057d48abd085fb4769dd2e7ebcb20e6a36c/Casks/bettertouchtool.rb"
       ];
